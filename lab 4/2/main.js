13c1b330d9bcee92f0f239fe7c89e55d0ea2c599
Alexander <80548017+TAMOTO24@users.noreply.github.com>
2023-09-26 21:11:50 +0300

    Delete lab 4 directory

--- lab 4/2/main.js
+++
@@ -1,9 +0,0 @@
-function toggleAccordion(Element) {
-    const icon = Element.querySelector('div [onclick="toggleAccordion(this)"]::after');
-    if(Element.nextElementSibling.style.display == 'block'){
-        Element.nextElementSibling.style.display = 'none';
-    }
-    else{
-        Element.nextElementSibling.style.display = 'block';
-    }
-}
